423d4329e74719b51fd1606c6fb3bf7154935569
Artos Web <teodor.at.tod@gmail.com>
2022-08-15 23:46:05 +0300

    edit navbar

--- air-bnb/src/App.js
+++ air-bnb/src/App.js
@@ -1,10 +1,11 @@
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-    <h1>opp</h1>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar />
+        </div>
+    );
 }
 
 export default App;
---
+++ air-bnb/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import logo from "../images/airbnb-logo.png"
+import "./Navbar.css"
+
+const Navbar = () => {
+    return (
+        <nav className="nav-logo">
+            <img src={logo} />
+        </nav>
+    );
+}
+
+export default Navbar;
