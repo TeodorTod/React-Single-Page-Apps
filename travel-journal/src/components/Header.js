919bd5661ca94c03d027fdabb4ce90fde532fba7
Artos Web <teodor.at.tod@gmail.com>
2022-08-21 09:50:16 +0300

    finish header

--- travel-journal/src/components/Header.js
+++ travel-journal/src/components/Header.js
@@ -4,7 +4,8 @@ import "./Header.css";
 export default function Header() {
     return (
         <div className="header-div">
-           <img src={earth} alt="earth-img" /> 
+           <img className="img-header" src={earth} alt="earth-img" />
+           <h1 className="header-h1">My travel journal</h1> 
         </div>
     );
 }
