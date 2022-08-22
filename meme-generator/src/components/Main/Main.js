681a2a035d5527d0766e3d8b00736ab397dff17c
Artos Web <teodor.at.tod@gmail.com>
2022-08-22 12:27:53 +0300

    add random meme

---
+++ meme-generator/src/components/Main/Main.js
@@ -0,0 +1,27 @@
+import "./Main.css"
+import memesData from "../../memesData";
+import { useState } from "react";
+
+export default function Main() {
+    const [image, setImage] = useState('');
+
+    const memeHandler = (e) => {
+        e.preventDefault();
+        const memesArr = memesData.data.memes;
+        const randomImage = (memesArr[Math.floor(Math.random() * memesArr.length)].url);
+        setImage(randomImage);
+    }
+    return (
+        <main>
+            <form>
+                <input className="first-input" type="text" placeholder="VIRUS SCAN?" />
+                <input className="second-input" type="text" placeholder="AINT NOBODY GOT TIME FOT THAT" />
+                <button className="button" onClick={memeHandler}>Get a new meme image 🖼</button>
+                <div>
+                    <img className="div-img" src={image} />
+                </div>
+
+            </form>
+        </main>
+    );
+}
