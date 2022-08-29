cf3d2df2f0113934092f988157abd7ee5898316d
Artos Web <teodor.at.tod@gmail.com>
2022-08-29 12:29:23 +0300

    add die component and styles

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import Die from './components/Die';
 
 function App() {
-  return (
-    <div>
-      <h1>opaaa</h1>
-    </div>
-  );
+    return (
+        <main>
+            <div className="dice-container">
+                <Die value="1" />
+                <Die value="3" />
+                <Die value="5" />
+                <Die value="2" />
+                <Die value="3" />
+                <Die value="4" />
+                <Die value="1" />
+                <Die value="2" />
+                <Die value="6" />
+                <Die value="1" />
+            </div>
+        </main>
+    );
 }
 
 export default App;
---
+++ tenzies/src/components/Die.js
@@ -0,0 +1,9 @@
+import './Die.css';
+
+export default function Die(props) {
+    return (
+        <div className="die-face">
+            <h2 className="die-num">{props.value}</h2>
+        </div>
+    );
+}
