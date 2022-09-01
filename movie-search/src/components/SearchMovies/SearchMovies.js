6cb223703a807f2fc044a310248754e733b82eec
Artos Web <teodor.at.tod@gmail.com>
2022-09-01 16:37:55 +0300

    add DisplayMovies component

--- movie-search/src/components/SearchMovies/SearchMovies.js
+++ movie-search/src/components/SearchMovies/SearchMovies.js
@@ -1,22 +1,48 @@
+import { useState } from "react";
+import DisplayMovies from "../DisplayMovies/DisplayMovies";
 import "./SearchMovies.css";
 
 export default function SearchMovies() {
+    const [query, setQuery] = useState('');
+    const [movies, setMovies] = useState([]);
+
     const searchMovies = async (e) => {
         e.preventDefault();
-        const query = "Jurassic Park";
 
         const url = `https://api.themoviedb.org/3/search/movie?api_key=1ffa60b61bc425ce36b91e23a2f41f3f&language=en-US&query=${query}&page=1&include_adult=false`;
 
-        const res = await fetch(url);
-        const data  = await res.json();
-
-        console.log(data);
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            setMovies(data.results);
+        } catch (err) {
+            console.log(err);
+        }
     }
+
+    const setFilm = (e) => {
+        setQuery(e.target.value);
+    }
+
     return (
-        <form className="form" onSubmit={searchMovies}>
-            <label className="label" htmlFor="query">Movie Name</label>
-            <input className="input" type="text" name="query" placeholder="i.e. Jurassic Park" />
-            <button className="button" type="submit">Search</button>
-        </form>
+        <>
+            <form className="form" onSubmit={searchMovies}>
+                <label className="label" htmlFor="query">Movie Name</label>
+                <input
+                    className="input"
+                    type="text"
+                    name="query"
+                    placeholder="i.e. Jurassic Park"
+                    value={query}
+                    onChange={setFilm}
+                />
+                <button className="button" type="submit">Search</button>
+            </form>
+            <div className="card-list">
+                {movies.filter(movie => movie.poster_path).map(movie => (
+                    <DisplayMovies movie={movie}/>
+                ))}
+            </div>
+        </>
     );
 }
